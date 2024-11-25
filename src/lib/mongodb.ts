c57f148b1543015acadf52abaeb492a76fafe2bb
MatchaTi <tehmatcha28@gmail.com>
2024-11-25 08:46:50 +0800

    aduhai again

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,14 +1,19 @@
 import mongoose from "mongoose";
 
+let isConnected = false;
+
 const connectMongoDB = async () => {
+  if (isConnected) {
+    console.log("Reusing existing MongoDB connection");
+    return;
+  }
   try {
     const uri = process.env.MONGODB_URI;
     if (!uri) {
-      throw new Error(
-        "MONGODB_URI environment variable is not defined in .env"
-      );
+      throw new Error("MONGODB_URI environment variable is not defined");
     }
     await mongoose.connect(uri);
+    isConnected = true;
     console.log("MongoDB connected");
   } catch (error) {
     console.error("MongoDB connection error:", error);
