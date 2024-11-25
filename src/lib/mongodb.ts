0b2e99bee81c7f8d1ff4fa764adbc0a3e40a8d4f
MatchaTi <tehmatcha28@gmail.com>
2024-11-25 08:41:48 +0800

    fix: bugggg againnnn

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,19 +1,12 @@
 import mongoose from "mongoose";
 
-let isConnected = false;
-
 const connectMongoDB = async () => {
-  if (isConnected) {
-    console.log("Reusing existing MongoDB connection");
-    return;
-  }
   try {
     const uri = process.env.MONGODB_URI;
     if (!uri) {
       throw new Error("MONGODB_URI environment variable is not defined");
     }
     await mongoose.connect(uri);
-    isConnected = true;
     console.log("MongoDB connected");
   } catch (error) {
     console.error("MongoDB connection error:", error);
