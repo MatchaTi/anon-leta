15c1010ca098373b250edf04f91c8df00b3977e3
MatchaTi <tehmatcha28@gmail.com>
2024-12-10 22:45:43 +0800

    chore: add share button

--- src/components/letterDetail.tsx
+++ src/components/letterDetail.tsx
@@ -1,5 +1,8 @@
-import { cn, formatDate } from "@/lib/utils";
+"use client";
+
+import { formatDate } from "@/lib/utils";
 import Link from "next/link";
+import { useState } from "react";
 
 interface LetterDetailProps {
   recipient: string;
@@ -23,12 +26,14 @@ const randomColors = [
   "bg-violet-200",
 ];
 
-const letterDetailStyles = () => {
-  const randomColor =
-    randomColors[Math.floor(Math.random() * randomColors.length)];
-
-  return cn(randomColor);
-};
+function hashStringToColor(input: string): string {
+  let hash = 0;
+  for (let i = 0; i < input.length; i++) {
+    hash = input.charCodeAt(i) + ((hash << 5) - hash);
+  }
+  const colorIndex = Math.abs(hash) % randomColors.length;
+  return randomColors[colorIndex];
+}
 
 export default function LetterDetail({
   recipient,
@@ -36,14 +41,28 @@ export default function LetterDetail({
   id_track,
   createdAt,
 }: LetterDetailProps) {
+  const [isCopied, setIsCopied] = useState(false);
+
+  const backgroundColor = hashStringToColor(recipient);
+
+  function copyLink() {
+    navigator.clipboard.writeText(window.location.href);
+    setIsCopied(true);
+    setTimeout(() => {
+      setIsCopied(false);
+    }, 3000);
+  }
+
   return (
     <section className='mt-24 mx-auto max-w-[814px] p-6'>
-      <h1 className='mb-12 text-4xl font-bold md:text-8xl'>Hai, {recipient}</h1>
-      <p className='mb-6 text-2xl'>
+      <h1 className='mb-6 text-4xl font-bold md:text-8xl md:mb-12'>
+        Hai, {recipient}
+      </h1>
+      <p className='mb-6 lg:text-2xl'>
         Ada seseorang yang mengirim untukmu sebuah pesan dan lagu, mereka ingin
         kamu untuk membaca dan mendengar lagunya yang mungkin relate :)
       </p>
-      <div className={`mb-6 p-8 shadow-xl ${letterDetailStyles()}`}>
+      <div className={`mb-6 p-8 shadow-xl ${backgroundColor}`}>
         <p className='mb-4 text-3xl md:text-5xl'>{description}</p>
         <iframe
           src={`https://open.spotify.com/embed/track/${id_track}?utm_source=generator`}
@@ -60,10 +79,20 @@ export default function LetterDetail({
         <p>Ungkapkan rasa tanpa nama, biarkan hati yang berbicara.</p>
         <Link
           href='/send'
-          className='py-4 px-8 bg-stone-900 text-stone-100 rounded-xl'
+          className='py-4 px-8 bg-stone-900 text-stone-100 rounded-xl hover:bg-stone-700 active:bg-stone-500'
         >
           Ungkapkan Rasa
         </Link>
+        <button
+          type='button'
+          disabled={isCopied}
+          onClick={copyLink}
+          className={`${
+            isCopied && "cursor-not-allowed"
+          } py-3 px-6 border-2 border-stone-900 bg-stone-100 rounded-xl hover:bg-stone-900 hover:text-stone-100`}
+        >
+          {isCopied ? "Link disalin!" : "Bagikan Pesan"}
+        </button>
       </div>
     </section>
   );
